d1bf4c46d1b80c25362882318b18ed4db200eea0
Patrick Avery <psavery@buffalo.edu>
2017-07-26 11:25:21 -0400

    Made our SCF into a function and added a test for it

--- QM_2017_SSS_Team1/scf.py
+++ QM_2017_SSS_Team1/scf.py
@@ -5,45 +5,17 @@ import psi4
 
 np.set_printoptions(suppress=True, precision=4)
 
-mol = psi4.geometry("""
-O
-H 1 1.1
-H 1 1.1 2 104
-""")
-
-mol.update_geometry()
-mol.print_out()
-
-e_conv = 1.e-6
-d_conv = 1.e-6
-
-n_el = 5
-
-bas = psi4.core.BasisSet.build(mol, target="sto-3g")
-
-mints = psi4.core.MintsHelper(bas)
-nbf = mints.nbf()
-
-if (nbf > 100):
-    raise Exception("More than 100 basis functions!")
-
-V = np.array(mints.ao_potential())
-T = np.array(mints.ao_kinetic())
-
-H = T + V
-
-S = np.array(mints.ao_overlap())
-g = np.array(mints.ao_eri())
-
-# Roothaan equations
-A = mints.ao_overlap()
-A.power(-0.5, 1.e-14)
-A = np.array(A)
-
-print(A @ S @ A)
-
 
 def diagonalize(F, A):
+    """
+    Diagonalize the Fock matrix
+
+    Args:
+        F: the Fock matrix
+        A: a unitary transformation matrix
+
+    Returns: eigenvalues, coefficients
+    """
     Fp = A.T @ F @ A
     eps, Cp = np.linalg.eigh(Fp)
     C = A @ Cp
@@ -51,49 +23,97 @@ def diagonalize(F, A):
 
 
 def calculate_density(F, A, n_el):
+    """
+    Calculate the electronic density
+
+    Args:
+        F: the Fock matrix
+        A: a unitary transformation matrix
+        n_el: the number of electrons
+
+    Returns: the electronic density
+    """
     eps, C = diagonalize(F, A)
     Cocc = C[:, :n_el]
     return Cocc @ Cocc.T
 
-D = calculate_density(H, A, n_el)
 
-E_old = 0.0
-F_old = None
-iteration = -1
-max_iter = 100
-while iteration < max_iter:
-    iteration += 1
+def calculate_basic_SCF_energy(mol, n_el, e_conv, d_conv, basis):
+    """
+    Calculate the energy of a molecule using a basic SCF algorithm
 
-    J = np.einsum("pqrs,rs->pq", g, D)
-    K = np.einsum("prqs,rs->pq", g, D)
+    Args:
+        mol: a psi4 mol with a geometry set
+        n_el: number of electrons
+        e_conv: energy convergence (usually around 1.e-6 eV)
+        d_conv: gradient convergence (usually around 1.e-6)
+        basis: a string with the basis set to use (such as 'sto-3g')
 
-    F = H + 2.0 * J - K
+    Returns: the total energy
+    """
 
-    grad = F @ D @ S - S @ D @ F
+    mol.update_geometry()
+    mol.print_out()
 
-    grad_rms = np.mean(grad ** 2) ** 0.5
+    bas = psi4.core.BasisSet.build(mol, target=basis)
 
-    E_electric = np.sum((F + H) * D)
-    E_total = E_electric + mol.nuclear_repulsion_energy()
+    mints = psi4.core.MintsHelper(bas)
+    nbf = mints.nbf()
 
-    E_diff = E_total - E_old
-    E_old = E_total
-    F_old = F
-    print(" %3d  %16.12f  %8.4e  %8.4e" %
-          (iteration, E_total, E_diff, grad_rms))
+    if (nbf > 100):
+        raise Exception("More than 100 basis functions!")
 
-    if E_diff < e_conv and grad_rms < d_conv:
-        print("Convergence reached!")
-        break
+    V = np.array(mints.ao_potential())
+    T = np.array(mints.ao_kinetic())
 
-    D = calculate_density(F, A, n_el)
+    H = T + V
 
-if iteration >= max_iter and (E_diff >= e_conv or grad_rms >= d_conv):
-    print("Failed to reach convergence!")
-else:
-    print("SCF has finished!")
+    S = np.array(mints.ao_overlap())
+    g = np.array(mints.ao_eri())
 
-psi4.set_options({"scf_type": "pk"})
-psi4_energy = psi4.energy("SCF/sto-3g", molecule=mol)
+    # Roothaan equations
+    A = mints.ao_overlap()
+    A.power(-0.5, 1.e-14)
+    A = np.array(A)
 
-print("Energy matches Psi4 %s" % np.allclose(psi4_energy, E_total))
+    print(A @ S @ A)
+
+    D = calculate_density(H, A, n_el)
+
+    E_old = 0.0
+    F_old = None
+    iteration = -1
+    max_iter = 100
+    while iteration < max_iter:
+        iteration += 1
+
+        J = np.einsum("pqrs,rs->pq", g, D)
+        K = np.einsum("prqs,rs->pq", g, D)
+
+        F = H + 2.0 * J - K
+
+        grad = F @ D @ S - S @ D @ F
+
+        grad_rms = np.mean(grad ** 2) ** 0.5
+
+        E_electric = np.sum((F + H) * D)
+        E_total = E_electric + mol.nuclear_repulsion_energy()
+
+        E_diff = E_total - E_old
+        E_old = E_total
+        F_old = F
+        print(" %3d  %16.12f  %8.4e  %8.4e" %
+              (iteration, E_total, E_diff, grad_rms))
+
+        if E_diff < e_conv and grad_rms < d_conv:
+            print("Convergence reached!")
+            break
+
+        D = calculate_density(F, A, n_el)
+
+    if iteration >= max_iter and (E_diff >= e_conv or grad_rms >= d_conv):
+        print("Failed to reach convergence!")
+    else:
+        print("SCF has finished!")
+
+    return E_total
--- tests/test_scf.py
+++ tests/test_scf.py
@@ -6,5 +6,31 @@ Tests for our SCF functions
 # We will test stuff here
 import pytest
 
-def test_nothing():
-    pass
+import numpy as np
+import psi4
+
+from QM_2017_SSS_Team1 import scf
+
+
+def test_calculate_basic_SCF_energy():
+    mol = psi4.geometry("""
+    O
+    H 1 1.1
+    H 1 1.1 2 104
+    """)
+
+    n_el = 5
+
+    e_conv = 1.e-6
+    d_conv = 1.e-6
+
+    basis = "sto-3g"
+
+    # Calculate the energy
+    E_total = scf.calculate_basic_SCF_energy(mol, n_el, e_conv, d_conv, basis)
+
+    # Now calculate the psi4 energy and make sure it matches
+    psi4.set_options({"scf_type": "pk"})
+    psi4_energy = psi4.energy("SCF/sto-3g", molecule=mol)
+
+    assert(np.allclose(psi4_energy, E_total))
