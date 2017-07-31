84f069c6ce0231a31800144d7bf3eadb10d4291f
Patrick Avery <psavery@buffalo.edu>
2017-07-31 17:26:23 -0400

    Added cpp functions for calculating J and k
    We can use the cpp function with PyBind11
    
    Also removed the now-unnecessary jk_algorithm_with_cpp.py file

---
+++ qm/jk_cpp.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import psi4
+
+import tensor_mult_numpy as tmn
+
+# Make sure we get the same random array
+np.random.seed(0)
+
+# A hydrogen molecule
+mol = psi4.geometry("""
+O
+H 1 1.1
+H 1 1.1 2 104
+""")
+
+# Build a ERI tensor
+basisname = "sto-3g"
+psi4.set_options({'basis': basisname,
+                  'scf_type': 'df',
+                  'e_convergence': 1e-10,
+                  'd_convergence': 1e-10})
+
+basis = psi4.core.BasisSet.build(mol, target=basisname)
+mints = psi4.core.MintsHelper(basis)
+I = np.array(mints.ao_eri())
+
+# Symmetric random density
+nbf = I.shape[0]
+D = np.random.rand(nbf, nbf)
+D = (D + D.T) / 2
+
+# Reference
+J_ref = np.einsum("pqrs,rs->pq", I, D)
+J = tmn.tensor_mult_numpy_J(I,D)
+
+K_ref = np.einsum("prqs,rs->pq", I, D)
+K = tmn.tensor_mult_numpy_K(I,D)
+
+print("K_ref is\n", K_ref)
+print("K is\n", K)
+
+############## OUR STUFF STARTS HERE ##########################
+# Your implementation
+#Get orbital basis from a Wavefunction object
+wfn = psi4.core.Wavefunction.build(
+        mol,
+        psi4.core.get_global_option('basis')
+)
+orb = wfn.basisset()
+
+#Build the complementary JKFIT basis for the aVDZ basis
+aux = psi4.core.BasisSet.build(mol, "DF_BASIS_SCF", fitrole="JKFIT",
+                               other=basisname)
+
+# The zero basis set
+zero_bas = psi4.core.BasisSet.zero_ao_basis_set()
+
+# Build instance of MintsHelper
+mints = psi4.core.MintsHelper(orb)
+
+# Build (P|pq) raw 3-index ERIs, dimension (1, Naux, nbf, nbf)
+Ppq = mints.ao_eri(zero_bas, aux, orb, orb)
+Ppq = np.squeeze(Ppq) # remove the 1-dimensions
+
+# Build and invert Coulomb metric, dimension (1, Naux, 1, Naux)
+metric = mints.ao_eri(zero_bas, aux, zero_bas, aux)
+metric.power(-0.5, 1.e-14)
+metric = np.squeeze(metric) # remove the 1-dimensions
+
+Qpq = np.einsum('QP,Ppq->Qpq', metric, Ppq)
+#print("metric is", metric)
+#print("Ppq is", Ppq)
+#print("Qpq is", Qpq)
+
+#print("metric.shape is", metric.shape)
+#print("Ppq.shape is", Ppq.shape)
+#print("Qpq.shape is", Qpq.shape)
+
+
+#J = np.random.rand(nbf, nbf)
+#K = np.random.rand(nbf, nbf)
+
+############## OUR STUFF ENDS HERE ##########################
+
+# Make sure your implementation is correct
+print("J is correct: %s" % np.allclose(J, J_ref))
+print("K is correct: %s" % np.allclose(K, K_ref))
---
+++ qm/tensor_mult_numpy.cpp
@@ -0,0 +1,159 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
+#include <string>
+#include <iostream>
+
+namespace py = pybind11;
+
+void print_arg(const std::string& s)
+{
+    std::cout << "String argument: " << s << std::endl;
+}
+
+// J = np.einsum("pqrs,rs->pq", g, D)
+py::array_t<double> tensor_mult_numpy_J(py::array_t<double>& t1,
+                                        py::array_t<double>& t2)
+{
+    py::buffer_info t1_info = t1.request();
+    py::buffer_info t2_info = t2.request();
+
+    const double* t1_data = static_cast<double*>(t1_info.ptr);
+    const double* t2_data = static_cast<double*>(t2_info.ptr);
+
+    size_t len_p = t1_info.shape[0];
+    size_t len_q = t1_info.shape[1];
+    size_t len_r = t1_info.shape[2];
+    size_t len_s = t1_info.shape[3];
+
+    std::vector<double> result(len_p * len_q);
+
+    for (size_t p = 0; p < len_p; ++p) {
+        for (size_t q = 0; q < len_q; ++q) {
+            double val = 0.0;
+            for (size_t r = 0; r < len_r; ++r) {
+                for (size_t s = 0; s < len_s; ++s) {
+                    val += t1_data[p * len_q * len_r * len_s + q * len_r * len_s + r * len_s + s] * t2_data[r * len_s + s];
+                }
+            }
+            result[p*len_q + q] = val;
+        }
+    }
+
+    py::buffer_info resbuf =
+        {
+            result.data(),
+            sizeof(double),
+            py::format_descriptor<double>::format(),
+            2,
+            { len_p, len_q },
+            { len_q * sizeof(double), sizeof(double) }
+        };
+
+
+    return py::array_t<double>(resbuf);
+}
+
+// K = np.einsum("prqs,rs->pq", g, D)
+py::array_t<double> tensor_mult_numpy_K(py::array_t<double>& t1,
+                                        py::array_t<double>& t2)
+{
+    py::buffer_info t1_info = t1.request();
+    py::buffer_info t2_info = t2.request();
+
+    const double* t1_data = static_cast<double*>(t1_info.ptr);
+    const double* t2_data = static_cast<double*>(t2_info.ptr);
+
+    size_t len_p = t1_info.shape[0];
+    size_t len_r = t1_info.shape[1];
+    size_t len_q = t1_info.shape[2];
+    size_t len_s = t1_info.shape[3];
+
+    std::vector<double> result(len_p * len_q);
+
+    for (size_t p = 0; p < len_p; ++p) {
+        for (size_t q = 0; q < len_q; ++q) {
+            double val = 0.0;
+            for (size_t r = 0; r < len_r; ++r) {
+                for (size_t s = 0; s < len_s; ++s) {
+                    val += t1_data[p * len_r * len_q * len_s + r * len_q * len_s + q * len_s + s] * t2_data[r * len_s + s];
+                }
+            }
+            result[p*len_q + q] = val;
+        }
+    }
+
+    py::buffer_info resbuf =
+        {
+            result.data(),
+            sizeof(double),
+            py::format_descriptor<double>::format(),
+            2,
+            { len_p, len_q },
+            { len_q * sizeof(double), sizeof(double) }
+        };
+
+
+    return py::array_t<double>(resbuf);
+}
+
+py::array_t<double> dgemm_numpy(double alpha,
+                                py::array_t<double> A,
+                                py::array_t<double> B)
+{
+    py::buffer_info A_info = A.request();
+    py::buffer_info B_info = B.request();
+
+    if (A_info.ndim != 2)
+        throw std::runtime_error("A is not a matrix");
+    if (B_info.ndim != 2)
+        throw std::runtime_error("B is not a matrix");
+
+    if (A_info.shape[1] != B_info.shape[0])
+        throw std::runtime_error("Rows of A != columns of B");
+
+    size_t C_nrows = A_info.shape[0];
+    size_t C_ncols = B_info.shape[1];
+    size_t n_k = A_info.shape[1]; // same as B_info.shape[0]
+
+    const double* A_data = static_cast<double *>(A_info.ptr);
+    const double* B_data = static_cast<double *>(B_info.ptr);
+
+    std::vector<double> C_data(C_nrows * C_ncols);
+
+    for (size_t i = 0; i < C_nrows; ++i) {
+        for (size_t j = 0; j < C_ncols; ++j) {
+            double val = 0.0;
+            for (size_t k = 0; k < n_k; ++k) {
+                val += A_data[i * n_k + k] * B_data[k * C_ncols + j];
+            }
+            val *= alpha;
+            C_data[i*C_ncols + j] = val;
+        }
+    }
+
+    py::buffer_info Cbuf =
+        {
+            C_data.data(),
+            sizeof(double),
+            py::format_descriptor<double>::format(),
+            2,
+            { C_nrows, C_ncols },
+            { C_ncols * sizeof(double), sizeof(double) }
+        };
+
+
+    return py::array_t<double>(Cbuf);
+}
+
+PYBIND11_PLUGIN(tensor_mult_numpy)
+{
+    py::module m("tensor_mult_numpy", "Tensor multiplication!");
+
+    m.def("print_arg", &print_arg, "Prints the passed arg");
+    m.def("tensor_mult_numpy_J", &tensor_mult_numpy_J);
+    m.def("tensor_mult_numpy_K", &tensor_mult_numpy_K);
+    m.def("dgemm_numpy", &dgemm_numpy);
+
+    return m.ptr();
+}
